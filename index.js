153b279474491e4d4e614b8b0307bf1f9dc8fe4f
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2020-10-01 23:04:03 +0200

    added json error handler

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const { promisify } = require('util')
 
 const authMiddleware = require('./auth')
 const initializeDatabase = require('./database')
+const jsonErrorHandler = async (err, req, res, next) => {
+  res.status(500).send({ error: err })
+}
 
 const app = express()
 app.use(bodyParser.json())
 app.use(authMiddleware)
+app.use(jsonErrorHandler)
 
 const startServer = async () => {
   await initializeDatabase(app)
--- readme.md
+++ readme.md
@@ -10,7 +10,7 @@ https://developer.okta.com/blog/2018/08/21/build-secure-rest-api-with-node
 
 ### Divergences
 1. epilogue did not play well with the newest equelize so it has been replaced with finalize
-2. 
+2. added JSON error handler to avoid  html errors
 
 ## Oauth2 Links:
 * https://resources.infosecinstitute.com/securing-web-apis-part-ii-creating-an-api-authenticated-with-oauth-2-in-node-js/
